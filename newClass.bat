fbfb5c528e5bbd9a711152e29ee1d8fd19471a2c
mrkamis <kamilsamiec@gmail.com>
2019-04-27 16:43:04 +0200

    concated  bat files

--- newClass.bat
+++ newClass.bat
@@ -12,10 +12,20 @@ ECHO ^<?php >%className%Controller.php
 ECHO %tab%require_once 'controller.php';>> %className%Controller.php
 ECHO %tab%class %className%Controller extends Controller ^{>> %className%Controller.php
 ECHO %tab%%tab%public function index() ^{>>%className%Controller.php
-ECHO %tab%%tab%%tab%echo '%className%Controller works!';>>%className%Controller.php
+ECHO %tab%%tab%%tab%$this-^>loadView();>>%className%Controller.php
 ECHO %tab%%tab%^}>>%className%Controller.php
 ECHO %tab%^}>>%className%Controller.php
 ECHO ?^>>>%className%Controller.php
 CD ./../..
-START .\newModel.bat %className%
+SET modelName=%1
+ECHO Creating model...
+CD ./app/models
+ECHO ^<?php >%modelName%Model.php
+ECHO %tab%require_once 'model.php';>> %modelName%Model.php
+ECHO %tab%class %modelName%Model extends Model ^{>> %modelName%Model.php
+ECHO %tab%^}>>%modelName%Model.php
+ECHO ?^>>>%modelName%Model.php
+CD ./../..
+CD ./app/views
+ECHO ^<h4^ class="container text-center"^>New Controller %className% works!^<^/h4^>>%className%View.phtml
 EXIT
