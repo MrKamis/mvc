3e38ff0c5320ffc830674c53d8fe831e4d1e9642
mrkamis <kamilsamiec@gmail.com>
2019-04-27 14:10:03 +0200

    added more features for ajax

--- ajax.php
+++ ajax.php
@@ -1,3 +1,9 @@
 <?php
-
+    header('Content-Type: text/plain');
+    if (file_exists(__DIR__ . '/app/ajax/' . $_GET['type'])) {
+        require_once __DIR__ . '/app/ajax/' . $_GET['type'];
+    } else {
+        echo '<b>File ' . $_GET['type'] . ' not exists!</b>';
+        exit;
+    }
 ?>
